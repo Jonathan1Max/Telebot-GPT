097d49af01807a84ef6e05bc5850017037868f7c
Bryanruizperez <125125063+Bryanruizperez@users.noreply.github.com>
2024-10-23 23:11:21 -0600

    cambios

--- Telebot-GPT-main/src/main/java/umg/edu/gt/Telebot/GPT/BotApp.java
+++ Telebot-GPT-main/src/main/java/umg/edu/gt/Telebot/GPT/BotApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  BotApp {
+public class BotApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BotApp.class, args);
--- src/main/java/umg/edu/gt/Telebot/GPT/Repository/ClientRepository.java
+++ src/main/java/umg/edu/gt/Telebot/GPT/Repository/ClientRepository.java
@@ -1,5 +1,9 @@
 package umg.edu.gt.Telebot.GPT.Repository;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import umg.edu.gt.Telebot.GPT.Model.Client;
@@ -9,12 +13,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Repository
 public class ClientRepository {
-
+    private static final Logger log = LoggerFactory.getLogger(ClientRepository.class);
     private static final String TABLENAME = "clients";
     private static DataSource dataSource;
 
@@ -25,7 +28,10 @@ public class ClientRepository {
 
 
     // metodo para agregar un nuevo cliente
-    public static void add(String name, Long chat_id) throws SQLException {
+    public static void add(@NotNull @Size(min=1,max = 100) String name,@NotNull Long chat_id) throws SQLException {
+        if(name == null || name.trim().isEmpty() || chat_id == null ){
+            throw new IllegalArgumentException("invalid input");
+        }
         String sql = "INSERT INTO " + TABLENAME + " (name, chat_id) VALUES (?, ?)";
         try (Connection connection = dataSource.getConnection();
              PreparedStatement stmt = connection.prepareStatement(sql)) {
@@ -38,13 +44,17 @@ public class ClientRepository {
     public static Client getById(Long chat_id) throws SQLException {
         String sql = "SELECT * FROM " + TABLENAME + " WHERE chat_id = ?";
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement stmt = connection.prepareStatement(sql)) {
+             PreparedStatement stmt = connection.prepareStatement(sql)){
             stmt.setLong(1, chat_id);
             ResultSet rs = stmt.executeQuery();
+
             if (rs.next()) {
+                Client client = new Client(rs.getInt("client"), rs.getString("name"));
+                log.info("client retrieved succesfully: {}",client);
                 return new Client(rs.getInt("client"), rs.getString("name"));
             }
         }
+        log.warn("client not found with  Chat ID = {}",chat_id);
         return null;  // Si no se encuentra el cliente
     }
 }
